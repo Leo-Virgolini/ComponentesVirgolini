ec7aaaf8c77128a31a530b0d39d1c97a81adde68
Leo <leo.virgolini@gmail.com>
2022-11-22 20:57:22 -0300

    primer entrega

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    ToolbarComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule
