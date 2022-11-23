51992305a165c934bef118d7945c9c86bdb52877
Leo <leo.virgolini@gmail.com>
2022-11-22 21:16:00 -0300

    primer entrega

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainComponent implements OnInit {
 
+  title: string = 'MAIN';
+
   constructor() { }
 
   ngOnInit(): void {
--- src/app/toolbar/toolbar.component.ts
+++ src/app/toolbar/toolbar.component.ts
@@ -7,9 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ToolbarComponent implements OnInit {
 
+  title: string = 'TOOLBAR';
+
   constructor() { }
 
   ngOnInit(): void {
   }
-
 }
