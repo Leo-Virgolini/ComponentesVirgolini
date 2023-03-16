9e53669d44f706b669dfabdc78d276a93d759273
Leo <leo.virgolini@gmail.com>
2023-03-15 22:36:19 -0300

    actualizado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { ToolbarComponent } from './toolbar/toolbar.component';
